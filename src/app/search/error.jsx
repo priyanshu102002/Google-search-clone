440df5c513184aa74f8247d9d92e8d5a9b417fba
priyanshukr449 <priyanshukr449@gmail.com>
2024-02-16 18:17:56 +0530

    error page added

---
+++ src/app/search/error.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+
+const error = ({ error, rest }) => {
+    useEffect(
+        (error, rest) => {
+            console.log("error", error);
+            console.log("rest", rest);
+        },
+        [error]
+    );
+    return <div className="flex flex-col items-center justify-center pt-10"><h1>Something went wrong!</h1>
+    <button className="text-blue-500">Try again</button></div>;
+};
+
+export default error;
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-const WebSearch = () => {
-  return (
-    <div>Web</div>
-  )
-}
+const WebSearchPage = async ({ searchParams }) => {
+    const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_API_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${searchParams.searchTerm}`
+    );
+    if (!response.ok) {
+        throw new Error("Something went wrong");
+    }
+    const data = await response.json();
+    const results = data.items;
 
-export default WebSearch
+    if (!results) {
+        return (
+            <div className="flex flex-col justify-center items-center pt-10">
+                <h1 className="text-3xl mb-4">
+                    No results found for <span className="text-red-500">{searchParams.searchTerm}</span>
+                </h1>
+                <p className="text-lg">
+                    Try searching the web or images for something else{" "}
+                    <Link href="/" className="text-blue-500">
+                        Home
+                    </Link>
+                </p>
+            </div>
+        );
+    }
+    return (
+        <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+    );
+};
+
+export default WebSearchPage;
