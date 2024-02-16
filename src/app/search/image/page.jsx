5d600209c6645914babc744cf534badbd795542b
priyanshukr449 <priyanshukr449@gmail.com>
2024-02-16 19:53:09 +0530

    image search result added

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import ImageSearchResult from "@/components/ImageSearchResult";
+import WebSearchResults from "@/components/WebSearchResults";
+import Link from "next/link";
+import React from "react";
 
-const ImageSearch = () => {
-  return (
-    <div>Image</div>
-  )
-}
+const ImageSearch = async ({ searchParams }) => {
+    const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_API_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+    );
+    if (!response.ok) {
+        throw new Error("Something went wrong");
+    }
+    const data = await response.json();
+    const results = data.items;
+
+    if (!results) {
+        return (
+            <div className="flex flex-col justify-center items-center pt-10">
+                <h1 className="text-3xl mb-4">
+                    No results found for{" "}
+                    <span className="text-red-500">
+                        {searchParams.searchTerm}
+                    </span>
+                </h1>
+                <p className="text-lg">
+                    Try searching the web or images for something else{" "}
+                    <Link href="/" className="text-blue-500">
+                        Home
+                    </Link>
+                </p>
+            </div>
+        );
+    }
+    return <div>{results && <ImageSearchResult results={data} />}</div>;
+};
 
 export default ImageSearch;
