9611747a37568c315219e830a9d3c87a5a6088c6
priyanshukr449 <priyanshukr449@gmail.com>
2024-03-03 18:48:34 +0530

    image tag problem resolved

--- src/components/ImageSearchResult.jsx
+++ src/components/ImageSearchResult.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import PaginationButton from "./PaginationButton";
-import Image from "next/image";
 
 export default function ImageSearchResults({ results }) {
     return (
@@ -10,7 +9,7 @@ export default function ImageSearchResults({ results }) {
                     <div className="mb-8" key={result.link}>
                         <div className="group">
                             <Link href={result.image.contextLink}>
-                                <Image
+                                <img
                                     src={result.link}
                                     alt={result.title}
                                     className="h-60 group-hover:shadow-xl w-full object-contain transition-shadow duration-300"
